b48f35bf3e882e70b8261624b2d832f6c7fb26d2
pdyrda <pdyrda@student.wszib.edu.pl>
2022-11-27 15:00:57 +0100

    Dodano zmianę w IDE

--- dowolny-skrypt.py
+++ dowolny-skrypt.py
@@ -1,3 +1,13 @@
+# pierwszy commit
 print("Oto dowolny skrypt Pythona:")
 a = 2 + 2
 print("2 + 2 =", a)
+
+# drugi commit
+
+b = 14196 % 3
+
+if b > 0:
+    print("Czy 14196 jest podzielne przez 3?", "Fałsz")
+else:
+    print("Czy 14196 jest podzielne przez 3?", "Prawda")
